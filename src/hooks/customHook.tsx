5ca6f34c74b7f6798aaa021213fd564fcd5f289f
Esumei Moses <archer.me24@gmail.com>
2023-03-30 08:44:26 +0100

    React using useState

---
+++ src/hooks/customHook.tsx
@@ -0,0 +1,59 @@
+import { useState, useEffect, createContext, useContext } from "react";
+/* 
+  Working with useContext in typescript
+*/
+interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attck: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+//custom hook
+function usePokemonSource() {
+  //:
+  //     pokemon: Pokemon[]; //not necessary just for type inference, also(:)
+  //  } {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetch("/pokemon.json")
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  }, []);
+
+  return { pokemon };
+}
+
+/*
+export const PokemonContext = createContext<
+ReturnType<typeof usePokemonSource>
+>({} as unknown as ReturnType<typeof usePokemonSource>);
+
+export function usePokemon() {
+    return useContext(PokemonContext); //to fix the destructuring in app.tsx in PokemonList;
+}
+*/
+//or
+export const PokemonContext = createContext<
+  ReturnType<typeof usePokemonSource> | undefined
+>(undefined);
+
+export function usePokemon() {
+  return useContext(PokemonContext)!; //to fix the destructuring in app.tsx in PokemonList;
+}
+
+export function PokemonProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <PokemonContext.Provider value={usePokemonSource()}>
+      {children}
+    </PokemonContext.Provider>
+  );
+}
+
+export default usePokemonSource;
