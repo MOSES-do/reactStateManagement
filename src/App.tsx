b97617a7ac64bee51c0691c26979644cac0f4f3b
Esumei Moses <archer.me24@gmail.com>
2023-03-30 10:53:56 +0100

    reducer Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import usePokemonSource from "./hooks/customHook";
-import {
-  PokemonContext,
-  PokemonProvider,
-  usePokemon,
-} from "./hooks/customHook";
+import { PokemonProvider, usePokemon } from "./hooks/customHook";
 
 function SearchBox() {
+  const { search, setSearch } = usePokemon();
+
   return (
     <input
       className="mt-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-800 focus:ring-indigo-800 sm:text-lg p-2"
       placeholder="Search"
-      value={""}
-      onChange={() => {}}
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
     />
   );
 }
--- src/hooks/customHook.tsx
+++ src/hooks/customHook.tsx
@@ -1,9 +1,10 @@
 import {
-  useState,
   useEffect,
   createContext,
   useContext,
   useReducer,
+  useCallback,
+  useMemo,
 } from "react";
 /* 
   Working with useContext in typescript
@@ -21,32 +22,70 @@ interface Pokemon {
 }
 
 //custom hook
-function usePokemonSource() {
-  //:
-  //     pokemon: Pokemon[]; //not necessary just for type inference, also(:)
-  //  } {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+function usePokemonSource(): {
+  pokemon: Pokemon[];
+  search: string;
+  setSearch: (search: string) => void;
+} {
+  type PokemonState = {
+    pokemon: Pokemon[];
+    search: string;
+  };
+
+  type PokemonAction =
+    | { type: "setPokemon"; payload: Pokemon[] }
+    | { type: "setSearch"; payload: string };
+
+  const [{ pokemon, search }, dispatch] = useReducer(
+    (state: PokemonState, action: PokemonAction) => {
+      switch (action.type) {
+        case "setPokemon":
+          return { ...state, pokemon: action.payload };
+        case "setSearch":
+          return { ...state, search: action.payload };
+      }
+    },
+    {
+      pokemon: [],
+      search: "",
+    }
+  );
 
   useEffect(() => {
     fetch("/pokemon.json")
       .then((response) => response.json())
-      .then((data) => setPokemon(data));
+      .then((data) =>
+        dispatch({
+          type: "setPokemon",
+          payload: data,
+        })
+      );
   }, []);
 
-  return { pokemon };
+  const setSearch = useCallback((search: string) => {
+    dispatch({
+      type: "setSearch",
+      payload: search,
+    });
+  }, []);
+
+  const filteredPokemon = useMemo(
+    () =>
+      pokemon
+        .filter((p) => p.name.toLowerCase().includes(search.toLowerCase()))
+        .slice(0, 21), // limit fetch pokemon to 20
+    [pokemon, search]
+  );
+
+  const sortedPokemon = useMemo(
+    () => [...filteredPokemon].sort((a, b) => a.name.localeCompare(b.name)),
+    [filteredPokemon]
+  );
+
+  return { pokemon: sortedPokemon, search, setSearch };
 }
 
-/*
-export const PokemonContext = createContext<
-ReturnType<typeof usePokemonSource>
->({} as unknown as ReturnType<typeof usePokemonSource>);
-
-export function usePokemon() {
-    return useContext(PokemonContext); //to fix the destructuring in app.tsx in PokemonList;
-}
-*/
-//or
-export const PokemonContext = createContext<
+const PokemonContext = createContext<
   ReturnType<typeof usePokemonSource> | undefined
 >(undefined);
 
