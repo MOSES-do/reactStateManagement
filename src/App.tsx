081e190def6603b1acd76661b4e8ca8649c490b4
Esumei Moses <archer.me24@gmail.com>
2023-04-04 04:45:15 +0100

    react location update

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,39 @@ import { PokemonProvider } from "./hooks/customHook";
 import { SearchBox } from "./components/SearchBox";
 import { PokemonList } from "./components/PokemonList";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Router,
+  useMatch,
+} from "@tanstack/react-location";
 
 const queryClient = new QueryClient();
 
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <SearchBox />
+        <PokemonList />
+      </>
+    ),
+  },
+];
+
 function App() {
   return (
-    <div className="mx-auto max-w-3xl">
-      <QueryClientProvider client={queryClient}>
-        <PokemonProvider>
-          <SearchBox />
-          <PokemonList />
-        </PokemonProvider>
-      </QueryClientProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <PokemonProvider>
+        <Router location={location} routes={routes}>
+          <div className="mx-auto max-w-3xl">
+            <Outlet />
+          </div>
+        </Router>
+      </PokemonProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/hooks/customHook.tsx
+++ src/hooks/customHook.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
 import {
-  useEffect,
   createContext,
   useContext,
   useReducer,
